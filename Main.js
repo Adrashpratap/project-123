8b7d6e9369dc5cf094aa8c00b1f0938653b15106
Adrashpratap <77541467+Adrashpratap@users.noreply.github.com>
2021-12-29 22:27:20 +0530

    Add files via upload

--- Main.js
+++ Main.js
@@ -1,3 +1,7 @@
+leftwirstX = 0;
+rightwristX = 0;
+diffrence = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550,500);
@@ -17,10 +21,18 @@ function modelLoaded()
 function gotPoses(results){
     if (results = length > 0) {
         console.log(results);
+
+        leftwirstX = results[0].pose.leftwirst.x;
+        rightwristX = results[0].pose.rightwrist.x;
+        diffrence = floor(leftwirstX-rightwristX);
+
+        console.log("leftwristX = "+ leftwirstX +"rightwristX = "+ rightwristX);
     }
 }
 
 function draw()
 {
     background("black"); 
+    text("Adarsh",50,400);
+    fill(255, 204, 0);
 }
